b46aeb0c4c4d7596794ba30af3643f3d037f6dbc
Max Eineder <max.eineder@googlemail.com>
2020-05-27 19:17:36 +0200

    Add project files.

---
+++ MqttClient.Blazor/Data/MqttWrapper.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MqttClient.Blazor.Data
+{
+    public class MqttWrapper
+    {
+        private IJSRuntime _js;
+        private DotNetObjectReference<MqttWrapper> _objRef;
+
+        public MqttWrapper(IJSRuntime js)
+        {
+            _js = js;
+            _objRef = DotNetObjectReference.Create(this);
+        }
+
+        public async Task InitializeAsync(string topic)
+        {
+            await _js.InvokeVoidAsync("mqttwrapper.createClient", _objRef, topic);
+        }
+
+        public async Task<string> SayHello()
+        {
+            return await _js.InvokeAsync<string>("mqttwrapper.sayHello", null);
+        }
+
+        [JSInvokable]
+        public void OnMessageArrived(string payloadString)
+        {
+            MessageReceived?.Invoke(this, payloadString);
+        }
+
+        public event EventHandler<string> MessageReceived;
+    }
+}
---
+++ MqttClient.Blazor/wwwroot/mqttwrapper.js
@@ -0,0 +1,41 @@
+﻿(function () {
+    window.mqttwrapper = {
+
+        sayHello: () => "hello",
+
+        createClient: (dotNetReference, topic) => {
+
+            // Create a client instance
+            client = new Paho.MQTT.Client("broker.mqttdashboard.com", 8000, "superclient_max_");
+
+            // set callback handlers
+            client.onConnectionLost = onConnectionLost;
+            client.onMessageArrived = onMessageArrived;
+
+            // connect the client
+            client.connect({ onSuccess: onConnect });
+
+            // called when the client connects
+            function onConnect() {
+                // Once a connection has been made, make a subscription and send a message.
+                console.log("onConnect");
+                client.subscribe(topic);
+            }
+
+            // called when the client loses its connection
+            function onConnectionLost(responseObject) {
+                if (responseObject.errorCode !== 0) {
+                    console.log("onConnectionLost:" + responseObject.errorMessage);
+                }
+            }
+
+            // called when a message arrives
+            function onMessageArrived(message) {
+                dotNetReference.invokeMethodAsync("OnMessageArrived", message.payloadString)
+                    .then(r => console.log("onMessageArrived:" + message.payloadString));
+                ;
+            }
+        }
+
+    };
+})();
